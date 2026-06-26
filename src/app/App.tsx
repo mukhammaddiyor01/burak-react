e09af65e2f82f852ad4a4ed33801cfe656d4b3ba
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-26 22:05:43 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../css/app.css";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { RippleBadge } from "./MaterialTheme/styled";
@@ -11,7 +10,9 @@ import { UserPage } from "./screens/userPage";
 import { HomeNavbar } from './components/headers/HomeNavbar';
 import { OtherNavbar } from './components/headers/OtherNavbar';
 import { Footer } from "./components/footer";
-
+import "../css/app.css";
+import "../css/navbar.css"
+import { HelpPage } from "./screens/helpPage";
 
 
 
@@ -33,7 +34,10 @@ function App() {
           </Route>
           <Route path="/member-page">
             <UserPage />
-          </Route>          
+          </Route>   
+          <Route path="/help">
+            <HelpPage />
+          </Route>       
           <Route path="/">
             <HomePage />
           </Route>
