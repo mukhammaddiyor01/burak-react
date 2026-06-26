bc9a4562622ac189a216a5b566b9f70ea7dcf7c4
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-26 20:28:05 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,35 +3,25 @@ import "../css/app.css";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { RippleBadge } from "./MaterialTheme/styled";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HomeNavbar } from './components/headers/HomeNavbar';
+import { OtherNavbar } from './components/headers/OtherNavbar';
+import { Footer } from "./components/footer";
 
 
 
 
 function App() {
-  return (
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/products">ProductsPage</Link>
-            </li>
-            <li>
-              <Link to="/orders">OrdersPage</Link>
-            </li>
-            <li>
-              <Link to="/member-page">UserPage</Link>
-            </li>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-          </ul>
-        </nav>
+  const location = useLocation();
+  console.log(location);
 
+  return (
+      <>
+        {location.pathname === "/" ? <HomeNavbar/> : <OtherNavbar />}
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
@@ -48,7 +38,8 @@ function App() {
             <HomePage />
           </Route>
         </Switch>
-      </div>
+        <Footer />
+      </>
   );
 }
 
