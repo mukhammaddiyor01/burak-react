524186b487ff8ebd7a2a4fc7a720928c6d6e2400
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-29 14:16:17 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -7,26 +7,16 @@ export function HomeNavbar() {
     const authMember = null;
     return (
     <div className="home-navbar">
-        <Container sx={{ mt: "55px", height: "642px" }}>
-            <Stack
-                sx={{ height: "50px"}}
-                flexDirection={"row"}
-                justifyContent= {"space-between"}
-                alignItems={"center"}
-            >
+        <Container className="navbar-container">
+            <Stack className="menu">
                 <Box>
                     <NavLink to="/">
                         <img 
-                        style={{width: "125px", height: "30px"}} 
+                        className="brand-logo" 
                         src="/icons/burak.svg"/>
                     </NavLink>
                 </Box>
-                <Stack 
-                flexDirection={"row"} 
-                justifyContent={"space-between"} 
-                minWidth={"700px"}
-                alignItems={"center"}
-                >
+                <Stack className="links">
                     <Box className={"hover-line"}>
                         <NavLink to="/" activeClassName={"underline"}>Home</NavLink>
                     </Box>
@@ -51,9 +41,15 @@ export function HomeNavbar() {
 
                     {!authMember ? (
                     <Box>
-                        <Button variant="contained" style={{backgroundColor: "#3776CC", color: "#f8f8ff"}}>Login</Button>
+                        <Button variant="contained" className="login-button" >
+                            Login
+                        </Button>
                     </Box>) : (
-                        <img />
+                        <img 
+                            className="user-avatar"
+                            src={"/icons/default-user.svg"}
+                            aria-haspopup={"true"}
+                        />
                     ) }
                 </Stack>
             </Stack>
