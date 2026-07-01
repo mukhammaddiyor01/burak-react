55c30fc33a70dc7f67d97ed0d211d50929033838
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-07-01 22:07:14 +0900

    fix: modify remaining files

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,47 +1,48 @@
-import { Container, Box, Stack } from "@mui/material";
-import React from "react";
-import Card from "@mui/joy/Card"
-import { CssVarsProvider, Typography } from '@mui/joy';
-import CardOverflow from '@mui/joy/CardOverflow';
-import AspectRatio from '@mui/joy/AspectRatio';
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
 
 const activeUsers = [
-    {memberNick: "Martin", memberImage: "/img/martin.webp"},
-    {memberNick: "Justin", memberImage: "/img/justin.webp"},
-    {memberNick: "Rose", memberImage: "/img/rose.webp"},
-    {memberNick: "Nusret", memberImage: "/img/nusret.webp"},
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
 ];
 
 export default function ActiveUsers() {
-    return (
+  return (
     <div className={"active-users-frame"}>
-        <Container>
-            <Stack className={"main"}>
-                <Box className={"category-title"}>Active Users</Box>
-                <Stack className={"cards-frame"}>
-                    <CssVarsProvider>
-                        {activeUsers.length !== 0 ? (
-                            activeUsers.map((ele, index) => {
-                                return (
-                                    <Card key={index} className={"card"}>
-                                        <CardOverflow>
-                                            <AspectRatio ratio="1">
-                                                <img src={ele.memberImage} alt={ele.memberNick} />
-                                            </AspectRatio>
-                                        </CardOverflow>
-                                        <Typography className={"member-nickname"}>
-                                            {ele.memberNick}
-                                        </Typography>
-                                    </Card>
-                                );
-                            })
-                        ) : (
-                            <Box className={"no-data"}>No Active Users!</Box>
-                        )}
-                    </CssVarsProvider>
-                </Stack>
-            </Stack>
-        </Container>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={ele.memberImage} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow>
+                        <Typography className={"member-nickname"}>
+                          {ele.memberNick}
+                        </Typography>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
-    );
+  );
 }
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import { Container } from "@mui/material";
+import React from "react";
 import Statistics from "./Statistics";
-import PopularDishes from './PopularDishes';
-import NewDishes from './NewDishes';
-import Advertisement from './Advertisement';
-import ActiveUsers from './ActiveUsers';
-import Events from './Events';
-import"../../../css/home.css"
-
-
-
-
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
+import "../../../css/home.css";
 
 export default function HomePage() {
-    return <div className={"homepage"}>
-        <Statistics />
-        <PopularDishes />
-        <NewDishes />
-        <Advertisement />
-        <ActiveUsers />
-        <Events />
-    </div>;
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
