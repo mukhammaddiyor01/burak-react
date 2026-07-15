75f5ab8f6c1ba1d9118edde5b319e85353f4cb21
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-07-15 22:02:41 +0900

    feat: create test class component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,6 +11,7 @@ import HelpPage from "./screens/helpPage";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import Test from "./screens/Test";
 
 function App() {
   const location = useLocation();
@@ -29,7 +30,9 @@ function App() {
           <UserPage />
         </Route>
         <Route path="/help">
-          <HelpPage />
+          <Test />
+
+          {/* <HelpPage /> */}
         </Route>
         <Route path="/">
           <HomePage />
