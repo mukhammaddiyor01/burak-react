ae6541ecab97cb48568be377cd6e7fa217d1a7e9
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-07-28 20:29:41 +0900

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -11,6 +11,7 @@ import { Messages } from "../../../lib/config";
 import { LoginInput, MemberInput } from "../../../lib/types/member";
 import MemberService from "../../services/MemberService";
 import { sweetErrorHandling } from "../../../lib/sweetAlert";
+import { useGlobals } from "../../hooks/useGlobals";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -50,7 +51,8 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
 
   const [memberNick, setMemberNick] = useState<string>("")
   const [memberPhone, setMemberPhone] = useState<string>("")
-  const [memberPassword, setMemberPassword] = useState<string>("")  
+  const [memberPassword, setMemberPassword] = useState<string>("")
+  const {setAuthMember} = useGlobals();
 
   const handleUsername = (e: T) => {
     console.log(e.target.value);
@@ -90,6 +92,7 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
       const result = await member.signup(signupInput);
 
       // Saving Authenticated User
+      setAuthMember(result);
       handleSignupClose();
     } catch(err) {
       console.log(err);
@@ -111,6 +114,7 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
       const member = new MemberService();
       const result = await member.login(loginInput);
 
+      setAuthMember(result);
       handleLoginClose();
     } catch(err) {
       console.log(err);
